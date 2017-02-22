035ea402bfdc6b64690f78e73ca99b4daa2417d1
Siva Reddy Chitukula <schituku@redhat.com>
2017-02-22 18:59:45 -0500

    Adding operators and calculator

---
+++ src/com/subha/operators/OperatorClass.java
@@ -0,0 +1,15 @@
+package com.subha.operators;
+
+public class OperatorClass {
+	
+	public void rangeInt(){
+		
+		int a = 200000000;
+		int b = 32000;
+		int c = 35000;
+		System.out.println("The value of a: "+a);
+		System.out.println("The value of b: "+b);
+		System.out.println("The value of c: "+c);
+	}
+
+}
---
+++ src/com/subha/operators/Variables.java
@@ -0,0 +1,13 @@
+package com.subha.operators;
+
+public class Variables {
+	
+	int instanceVariable;
+	static int classVariable;
+	
+	public void localVariable(){
+		int localVar = 5;
+		System.out.println("The value of local variable "+localVar);
+	}
+	
+}
